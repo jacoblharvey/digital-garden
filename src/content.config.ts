8c17d9efdd3df3b03653fe7dce0b19d7cd7c10fe
Jacob Harvey <jacoblharvey@outlook.com>
2025-04-27 12:56:03 -0500

    Organize imports

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { glob } from "astro/loaders";
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const journal = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/journal" }),
