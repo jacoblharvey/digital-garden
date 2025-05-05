95b445f58b88a7f93c207fdf56d5b65972c75a92
Jacob Harvey <jacoblharvey@outlook.com>
2025-05-04 21:44:21 -0500

    Add reading time to journal entries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/scripts/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacoblharvey.com",
   trailingSlash: "never",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
