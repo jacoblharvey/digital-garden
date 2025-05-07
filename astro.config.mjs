334928af48f2ebf7e19c1bede3617d4f90e886c5
Jacob Harvey <jacoblharvey@outlook.com>
2025-05-07 13:33:36 -0500

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-// @ts-check
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/scripts/remark-reading-time.mjs";
 
 // https://astro.build/config
+// https://astro.build/integrations/
 export default defineConfig({
   site: "https://jacoblharvey.com",
   trailingSlash: "never",
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
+
+  integrations: [sitemap()],
 });
